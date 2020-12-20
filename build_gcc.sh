4f0a52742d6e44b6d7c4da72597cb18aa067f3c4
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2020-12-20 10:29:04 +0700

    Fix coding style

--- build_gcc.sh
+++ build_gcc.sh
@@ -9,8 +9,6 @@
 # We need this directive
 # shellcheck disable=1090
 
-
-##### Build Env Dependencies
 tg_sendinfo() { 
         curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
         -d chat_id="$CHAT_ID" \
@@ -27,60 +25,52 @@ tg_postinfo() {
         -d text="Job <code>BakingCC</code> completed and pushed to%0A\<a href='https://github.com/Reinazhard/gcc.git'>Repo</a>%0A%0A@eve_enryu Check it plx!"
 }
 
-
-
-build_env()
-{
-export CHAT_ID="-1001403511595"
-tg_sendinfo
-git clone https://github.com/crosstool-ng/crosstool-ng
-cd crosstool-ng
-./bootstrap
-./configure
-make -j$(nproc --all)
-make install
-cd ..
-rm -rf crosstool-ng
-HOME_DIRR=$PWD
-export HOME_DIRR
-echo "Build Dependencies Installed....."
-}
-##### Build Configs
-
-build_conf()
-{
-mkdir repo
-cd repo
-git config --global user.email "muh.alfarzoy@gmail.com"
-git config --global user.name "Reinazhard"
+build_env() {
+        export CHAT_ID="-1001403511595"
+        tg_sendinfo
+        git clone https://github.com/crosstool-ng/crosstool-ng
+        cd crosstool-ng
+        ./bootstrap
+        ./configure
+        make -j$(nproc --all)
+        make install
+        cd ..
+        rm -rf crosstool-ng
+        HOME_DIRR=$PWD
+        export HOME_DIRR
+        echo "Build Dependencies Installed....."
 }
 
-run()
-{
-echo "Starting build!"
-git clone https://github.com/najahiiii/Noob-Script.git -b noob config 
-cd config
-mv tc-configs/defconfig_aarch64-elf defconfig 
-ct-ng defconfig
-ct-ng show-tuple
-ct-ng build 
-echo "Build finished!"
+build_conf() {
+        mkdir repo
+        cd repo
+        git config --global user.email "muh.alfarzoy@gmail.com"
+        git config --global user.name "Reinazhard"
 }
 
-##### Here's the blecc megik
-push_gcc()
-{
-chmod -R 777 "$HOME"/x-tools
-cd "$HOME"/x-tools/
-git clone https://$GH_TOKEN@github.com/silont-project/aarch64-elf-gcc.git push
-cd push
-rm -r *
-mv ../aarch64-elf/* .
-git add .
-git commit -m "[DroneCI]: GCC-11 $(date +%d%m%y)" --signoff
-git push -q https://$GH_TOKEN@github.com/silont-project/aarch64-elf-gcc.git 11.x 
-tg_postinfo
-echo "Job Successful!"
+run() {
+        echo "Starting build!"
+        git clone https://github.com/najahiiii/Noob-Script.git -b noob config 
+        cd config
+        mv tc-configs/defconfig_aarch64-elf defconfig 
+        ct-ng defconfig
+        ct-ng show-tuple
+        ct-ng build 
+        echo "Build finished!"
+}
+
+push_gcc() {
+        chmod -R 777 "$HOME"/x-tools
+        cd "$HOME"/x-tools/
+        git clone https://$GH_TOKEN@github.com/silont-project/aarch64-elf-gcc.git push
+        cd push
+        rm -r *
+        mv ../aarch64-elf/* .
+        git add .
+        git commit -m "[DroneCI]: GCC-11 $(date +%d%m%y)" --signoff
+        git push -q https://$GH_TOKEN@github.com/silont-project/aarch64-elf-gcc.git 11.x 
+        tg_postinfo
+        echo "Job Successful!"
 }
 
 build_env
